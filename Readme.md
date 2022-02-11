75d7814643ba8dd99c2c14628ea98c2e8e21b16a
JaeXploit <wwwsibontotcom91@gmail.com>
2022-02-11 19:02:14 +0700

    public

---
+++ Readme.md
@@ -0,0 +1,30 @@
+### FOR COMMAND
+```sh
+-> apt update
+-> apt upgrade -y
+-> apt install git -y
+```
+### FOR SQLMAP
+```sh
+-> cd sqlmap
+```
+### FOR DDOS
+```sh
+-> cd ddos
+```
+### FOR BOT WA
+```sh
+-> cd bot-wa2
+```
+### Ada pertanyaan?
+```sh
+-> Kebingungan cara pasang Tools nya atau mungkin mengalami masalah? 🤔
+-> Silahkan tanya mengenai masalahmu ke Pengembang!
+=> [`BANTUAN PENGGUNA`](https://chat.whatsapp.com/LMDWmqNiFLoB1axbTO3Ume)
+=> [`BANTUAN PENGGUNA`](https://chat.whatsapp.com/LMDWmqNiFLoB1axbTO3Ume)
+```
+### NOTE
+```sh
+JARINGAN KUDU CEPAT
+```
+
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+apt-get update && apt-get upgrade -y && apt-get install git -y && apt-get install nodejs && apt-get install python && apt-get install python2 -y && apt-get install ffmpeg -y && apt-get install libwebp -y && apt-get install imagemagick -y  && apt-get install figlet -y && apt-get install git -y && apt-get install bash -y && npm install -g pm2 -y   && npm install -g npm -y && pip install -r req.txt
+git clone https://github.com/JaeXploit/sqlmap
+git clone https://github.com/JaeXploit/ddos
+git clone https://github.com/JaeXploit/bot-wa2
+cd
+ls
+figlet DONE
